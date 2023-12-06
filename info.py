716dd9358bf3f7dcafe37ce8e7320fe4df8ead3f
Filterbot-only <147235633+Filterbot-only@users.noreply.github.com>
2023-12-06 09:16:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mrmalik_offl:Shahulshaji10@msrenamer.px3s58x.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vairamdonpre827:vairamdonpre82710@cluster0.s7jz2d9.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "mrmalik_offl")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
